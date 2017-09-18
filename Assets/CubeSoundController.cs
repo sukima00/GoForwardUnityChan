82c4cb25d7fc315f261ba7def46b77729eac33a9
筒井　匠 <noltyska0883@gmail.com>
2017-09-18 21:49:02 +0900

    Lesson7　提出課題：キューブの接触時に効果音をつけましょう
    Lesson7で作成したGoForwardUnityChanをもとに作成しました

---
+++ Assets/CubeSoundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSoundController : MonoBehaviour {
+    private AudioSource audioSource;
+    public AudioClip sound;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = sound;
+        audioSource.loop = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        audioSource.Play();
+    }
+}
---
+++ Assets/CubeSoundController2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSoundController2 : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public AudioClip block;
+
+    // Use this for initialization
+    void Start () {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = block;       // 鳴らす音(変数)を格納.
+        audioSource.loop = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CubeTag" || other.gameObject.tag == "groundTag")
+        {
+            audioSource.Play();
+        }
+    }
+}
